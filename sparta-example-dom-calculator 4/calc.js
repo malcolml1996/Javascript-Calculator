de8cb466f8a10c59f61ae957777c268144974850
malcolmlindsayspartaglobal1996 <42536901+malcolmlindsayspartaglobal1996@users.noreply.github.com>
2018-08-30 10:33:32 +0100

    Add files via upload

--- sparta-example-dom-calculator 4/calc.js
+++ sparta-example-dom-calculator 4/calc.js
@@ -1,43 +1,42 @@
 // alert("hi");
-//
- window.onload = function ()
+// //
 
- {
+var display = document.getElementById('screen');
+
   var buttons = document.getElementsByTagName('button');
-  var screena = document.getElementsByTagName("h2");
- }
- for (var i=0; i<buttons.length; i++);
-if (buttons[i].innerHTML == 'AC') {
-  buttons[i].addEventListener('click', clearScreen());
-}
-else if (buttons[i].innerHTML == '=') {
-  buttons[i].addEventListener('click',calculate());
-}
-else {
-buttons[i].addEventListener('click',printscreen(buttons[i].innerHTML));
-}
-}
 
-function printScreen(string);{
-return function() {
-  screen.innerHTML += string;
-}
-}
+  var screen = document.querySelector("h1");
 
-function clearScreen() {
-  return function() {
-    screen.innerHTML = "";
+   for (var i=0; i<buttons.length; i++) {
+    if (buttons[i].innerHTML === 'AC') {
+      buttons[i].addEventListener('click', clearScreen());
+    }
+    else if (buttons[i].innerHTML === '=') {
+      buttons[i].addEventListener('click', calculate());
+    }
+    else {
+      buttons[i].addEventListener('click', printScreen(buttons[i].innerHTML));
+    }
   }
-}
 
-function calculate() {
-return function () {
-  try {
-    screen.innerHTML = eval(screen.innerHTML);
+
+  function printScreen(string){
+    return function() {
+      screen.innerHTML += string;
+    }
   }
-  catch {
-    screen.HTML = "math error!";
+
+  function clearScreen() {
+    return function() {
+      screen.innerHTML = " ";
+    }
   }
-};
-}
-};
+  function calculate() {
+      return function () {
+
+          screen.innerHTML = eval(screen.innerHTML);
+
+        }
+
+
+  }
